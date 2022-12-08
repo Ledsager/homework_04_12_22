25c6f4256dae0a8a4c8e7ed514c8f92bea55cd63
WIN-JJH83PJTM7J\mkadmin <r.yakupov81@yandex.ru>
2022-12-08 15:43:29 +0300

    поправил код

--- task_01.java
+++ task_01.java
@@ -1,9 +1,12 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,8 +15,7 @@ public class task_01 {
         
         File file = new File("demo.txt");
         List<String> listOfStrings = new ArrayList<String>();
-        try (BufferedReader br = new BufferedReader(new FileReader(file)))
-        {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))){
             String line;
             while ((line = br.readLine()) != null) {
                 listOfStrings.add(line);                                    
@@ -24,6 +26,7 @@ public class task_01 {
             System.out.println();
             
         }
+
         String[] array = listOfStrings.toArray(new String[0]);
         double[] totalAmount = new double[array.length];
         
@@ -34,29 +37,62 @@ public class task_01 {
             i+=1;
         }
 
-        try (DataOutputStream fw = new DataOutputStream(new FileOutputStream("result.txt"))) {
-            
-            for ( i = 0; i <= (array.length-2); i+=2) {
+        // File file = new File("result.txt");
+        // String text = "sample text";
+        File file1 = new File("result.txt");
+        try {
+                PrintWriter out = new PrintWriter(file1);
+                for ( i = 0; i <= (array.length-2); i+=2) {
                 // System.out.println(i);
-                if ((totalAmount[i]!=0)&(totalAmount[i+1]!=0)&(i%2==0)&((i+1)%2!=0)) {
-                    System.out.println(totalAmount[i+1]);
-                    System.out.println(totalAmount[i]);
-                    System.out.println(i);
+                if ((totalAmount[i]!=0)&(totalAmount[i+1]!=0)) {
+                    // System.out.println(totalAmount[i+1]);
+                    // System.out.println(totalAmount[i]);
+                    // System.out.println(i);
                     double result = Math.pow(totalAmount[i+1],totalAmount[i]);
 
-                    fw.writeDouble(result);
+                    out.println(result);
+                    // out.append(result);
+                    System.out.println("Successfully written data to the file");
                     // System.out.println(result);
                 }else {
-                    System.out.println(totalAmount[i+1]);
-                    System.out.println(totalAmount[i]);
-                    System.out.println(i);
-                    fw.writeUTF("не определено");
+                    // System.out.println(totalAmount[i+1]);
+                    // System.out.println(totalAmount[i]);
+                    // System.out.println(i);
+                    out.print("не определено");
+                    System.out.println("Successfully written data to the file");
+                    
                 }
-
+                out.close();
             }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            }
+                // out.print(text);
+                // System.out.println("Successfully written data to the file");
+            catch (IOException e) {
+            e.printStackTrace();
         }
+        // try (FileWriter fw = new FileWriter("result.txt", false)) {
+            
+        //     for ( i = 0; i <= (array.length-2); i+=2) {
+        //         // System.out.println(i);
+        //         if ((totalAmount[i]!=0)&(totalAmount[i+1]!=0)) {
+        //             System.out.println(totalAmount[i+1]);
+        //             System.out.println(totalAmount[i]);
+        //             System.out.println(i);
+        //             double result = Math.pow(totalAmount[i+1],totalAmount[i]);
+
+        //             fw.write(result);
+        //             // System.out.println(result);
+        //         }else {
+        //             System.out.println(totalAmount[i+1]);
+        //             System.out.println(totalAmount[i]);
+        //             System.out.println(i);
+        //             fw.println("не определено");
+        //         }
+        //         fw.close();
+        //     }
+        // } catch (IOException ex) {
+        //     System.out.println(ex.getMessage());
+        // }
     
         System.out.println(listOfStrings);
     }

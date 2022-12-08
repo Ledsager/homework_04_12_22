11a463efb7f7fc19bfa3dc178ce77a050c6122f0
Ledsager <r.yakupov81@yandex.ru>
2022-12-09 01:18:03 +0300

    решение надо доработать

--- task_01.java
+++ task_01.java
@@ -1,64 +1,52 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class task_01 {
     public static void main(String[] args) {
-        
         File file = new File("demo.txt");
         List<String> listOfStrings = new ArrayList<String>();
-        try (BufferedReader br = new BufferedReader(new FileReader(file)))
-        {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
-                listOfStrings.add(line);                                    
+                listOfStrings.add(line);
             }
-            
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println();
-            
         }
         String[] array = listOfStrings.toArray(new String[0]);
         double[] totalAmount = new double[array.length];
-        
         int i = 0;
         for (String item : array) {
             String[] itemInfo = item.split(" ");
             totalAmount[i] = Double.parseDouble(itemInfo[1]);
-            i+=1;
+            i += 1;
         }
-
         try (
-            FileOutputStream fw = new FileOutputStream("result.txt", true)) {
-            for ( i = 0; i <= (array.length-2); i+=2) {
-                // System.out.println(i);
-                if ((totalAmount[i]!=0)&(totalAmount[i+1]!=0)&(i%2==0)&((i+1)%2!=0)) {
-                    System.out.println(totalAmount[i+1]);
-                    System.out.println(totalAmount[i]);
-                    System.out.println(i);
-                    Double result = Math.pow(totalAmount[i+1],totalAmount[i]);
-                    byte byteValue = result.byteValue();
-                    fw.write(byteValue);
-                    // System.out.println(result);
-                }else {
-                    System.out.println(totalAmount[i+1]);
-                    System.out.println(totalAmount[i]);
-                    System.out.println(i);
+                FileWriter fw = new FileWriter("result.txt", StandardCharsets.UTF_8)) {
+            for (i = 0; i <= (array.length - 2); i += 2) {
+                if ((totalAmount[i] != 0) || (totalAmount[i + 1] != 0)) {
+                    Double result = Math.pow(totalAmount[i + 1], totalAmount[i]);
+                    String stringresult = Double.toString(result);
+                    fw.append(stringresult);
+                    fw.append("\n");
+                    fw.flush();
+                } else {
                     String text = "не определенно\n";
-                    byte buf[] = text.getBytes();
-                    fw.write(buf);
+                    fw.write(text);
+                    fw.flush();
                 }
-
             }
+            fw.close();
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-    
         System.out.println(listOfStrings);
     }
 }
